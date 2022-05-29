71c8bf14f83d235ce4021ccb1196c27697784f21
Zaida Hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-05-29 16:13:35 +0000

    edited code

--- js/script.js
+++ js/script.js
@@ -5,8 +5,45 @@
 /**
  * Check servie worker.
  */
- if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-Unit6-03-JS/sw.js", {
-    scope: "/ICS2O-Unit6-03-JS/",
-  })
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS20-Unit6-03-JS/sw.js", {
+    scope: "/ICS20-Unit6-03-JS/",
+  });
 }
+
+/**
+ * Get API info.
+ */
+// code from: https://www.youtube.com/watch?v=670f71LTWpM
+
+/**
+ * Get API info.
+ */
+const getWeather = async (URLAddress) => {
+  try {
+    const request = await fetch(URLAddress);
+    const jsonData = await request.json();
+    var tempKalvin = jsonData.main.temp;
+    var tempCelsius = 0;
+    const feeling = jsonData.weather[0];
+    const image = feeling.icon;
+
+    console.log(jsonData.weather);
+    document.getElementById("weather-image").innerHTML =
+      "<img src='http://openweathermap.org/img/wn/" +
+      image +
+      "@2x.png' alt='Weather Icon' width='10%'><br><h5>";
+    (">");
+
+    // Calculates from Kalvin to Celsius
+    tempCelsius = tempKalvin - 273.15;
+
+    document.getElementById("weather-api").innerHTML =
+      "The current weather is " + tempCelsius.toFixed(2) + " °C";
+  } catch (err) {
+    console.log(err);
+  }
+};
+getWeather(
+  "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
+);
